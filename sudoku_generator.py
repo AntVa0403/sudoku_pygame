b95cef98da2f1f62799decdd673fe919ff347d1f
AntVa0403 <anthonyvargas0403@gmail.com>
2025-11-25 00:02:58 -0500

    Update sudoku_generator.py
    I updated a few of the functions, let me know how it looks

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -23,7 +23,8 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        pass
+        self.row_length=row_length
+		self.removed_cells=removed_cells
 
     '''
 	Returns a 2D python list of numbers which represents the board
@@ -32,7 +33,7 @@ class SudokuGenerator:
 	Return: list[list]
     '''
     def get_board(self):
-        pass
+        return self.board
 
     '''
 	Displays the board to the console
@@ -42,7 +43,19 @@ class SudokuGenerator:
 	Return: None
     '''
     def print_board(self):
-        pass
+        def print_board(self):
+    for r in range(9):
+        if r % 3 == 0 and r != 0:
+            print("-" * 21)
+
+        for c in range(9):
+            if c % 3 == 0 and c != 0:
+                print("|", end=" ")
+
+            value = self.board[r][c]
+            print(value if value != 0 else ".", end=" ")
+
+        print()
 
     '''
 	Determines if num is contained in the specified row (horizontal) of the board
@@ -55,7 +68,8 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_row(self, row, num):
-        pass
+    return num not in self.board[row]
+
 
     '''
 	Determines if num is contained in the specified column (vertical) of the board
@@ -68,7 +82,11 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_col(self, col, num):
-        pass
+    for r in range(9):
+        if self.board[r][col] == num:
+            return False
+    return True
+
 
     '''
 	Determines if num is contained in the 3x3 box specified on the board
